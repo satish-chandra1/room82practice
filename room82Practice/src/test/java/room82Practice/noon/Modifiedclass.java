ec7c2cdb81d37cb955b210a158c5f42f25b0cf9c
mrinm <mrinm@MRINMOY>
2022-03-26 17:35:05 +0530

    please watch again

--- room82Practice/src/test/java/room82Practice/noon/Modifiedclass.java
+++ room82Practice/src/test/java/room82Practice/noon/Modifiedclass.java
@@ -1,10 +1,16 @@
 package room82Practice.noon;
 
 import org.testng.annotations.Test;
+/**
+ * This class is for learning movies name
+ * @author mrinm
+ *
+ */
 
 public class Modifiedclass {
 @Test
 public void movies() {
 	System.out.println("Inception");
+	System.out.println("Hen....t..a...iii");
 }
 }
