33ee8e1fc76fb989e12b173863fdab572e4a6ef6
mrinm <mrinm@MRINMOY>
2022-03-26 15:15:48 +0530

    First commit by mrin

---
+++ room82Practice/src/test/java/room82Practice/noon/Mrin.java
@@ -0,0 +1,20 @@
+package room82Practice.noon;
+
+import org.testng.annotations.Test;
+
+public class Mrin {
+	@Test
+	public void create() {
+		System.out.println("Mrin stays away from girls");
+	}
+	@Test
+	public void created() {
+		System.out.println("Mrin stays away from boys");
+	}
+	
+	@Test
+	public void creates() {
+		System.out.println("Another method");
+	}
+
+}
