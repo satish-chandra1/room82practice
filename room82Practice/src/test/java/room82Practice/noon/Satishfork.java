3327094fd7b92e358acdaf76793528b2e1be0eed
Sumit Pathak <Sumit Pathak@LAPTOP-S5GSA3HC>
2022-03-26 17:06:03 +0530

    changes done

--- room82Practice/src/test/java/room82Practice/noon/Satishfork.java
+++ room82Practice/src/test/java/room82Practice/noon/Satishfork.java
@@ -1,11 +1,16 @@
 package room82Practice.noon;
 
 import org.testng.annotations.Test;
-
+/**
+ * 
+ * @author Sumit Pathak
+ *
+ */
 public class Satishfork {
 	@Test
 	public void practicefork() {
 		System.out.println("fork");
+		System.out.println("added");
 	}
 	
 
